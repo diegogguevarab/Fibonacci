a79568f2722d4978f78ac29883af6563c75f374c
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-08-16 16:39:48 -0500

    Actividad web terminada

--- app/src/main/java/com/taller/fibonacci/Web.java
+++ app/src/main/java/com/taller/fibonacci/Web.java
@@ -2,6 +2,7 @@ package com.taller.fibonacci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class Web extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Web extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
+        WebView myWebView = findViewById(R.id.webview);
+        myWebView.loadUrl("https://en.wikipedia.org/wiki/Fibonacci");
     }
 }
