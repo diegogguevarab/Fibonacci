c858c011fc99fe6e78fa98426ccc632657f795c2
Diego Gustavo Guevara Bernal <diegogb0113@gmail.com>
2019-08-21 10:51:49 -0500

    Factorial terminado

--- app/src/main/java/com/taller/fibonacci/Factorial.java
+++ app/src/main/java/com/taller/fibonacci/Factorial.java
@@ -2,6 +2,7 @@ package com.taller.fibonacci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Factorial extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class Factorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_factorial);
+        TextView txt = findViewById(R.id.factorialTxt);
+        String multi= "";
+        for(int i=1; i<getIntent().getIntExtra("num",0);i++)
+        {
+            multi+= (i)+"*";
+        }
+        multi+=getIntent().getIntExtra("num",0);
+        txt.setText("Multiplicación: "+multi+"\nResultado: "+factorial(getIntent().getIntExtra("num",0)));
+    }
+    static int factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        return n*factorial(n-1);
     }
 }
